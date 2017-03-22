908a734ff8e60bc16a2f2ca87c54e46034f303d9
Alijon <alijonshukurov@gmail.com>
2017-03-22 14:39:26 +0300

    bug fix

--- index.js
+++ index.js
@@ -4,37 +4,29 @@ var fs = require('fs');
 var $rdf = require('rdflib');
 var async = require('async');
 
-var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-var RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#")
-var FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/")
-var XSD = $rdf.Namespace("http://www.w3.org/2001/XMLSchema#")
-
-var store = new $rdf.IndexedFormula();
-
 var urls = [
-	'http://nanojournal.ifmo.ru/news/%D0%BC%D0%B8%D0%BD%D0%BE%D0%B1%D1%80%D0%BD%D0%B0%D1%83%D0%BA%D0%B8-%D1%80%D0%B0%D0%B7%D1%8A%D1%8F%D1%81%D0%BD%D0%B8%D0%BB-%D1%81%D0%B8%D1%82%D1%83%D0%B0%D1%86%D0%B8%D1%8E-%D1%81-web-of-science/',
-	'http://nanojournal.ifmo.ru/news/59/',
-	'http://nanojournal.ifmo.ru/news/57/',
-	'http://nanojournal.ifmo.ru/news/50/',
-	'http://nanojournal.ifmo.ru/news/44/',
-	'http://nanojournal.ifmo.ru/news/41/',
-	'http://nanojournal.ifmo.ru/news/40/',
-	'http://nanojournal.ifmo.ru/news/37/',
-	'http://nanojournal.ifmo.ru/news/36/',
-	'http://nanojournal.ifmo.ru/news/34/',
-	'http://nanojournal.ifmo.ru/news/33/',
-	'http://nanojournal.ifmo.ru/news/28/',
-	'http://nanojournal.ifmo.ru/news/26/',
-	'http://nanojournal.ifmo.ru/news/21/',
-	'http://nanojournal.ifmo.ru/news/17/',
-	'http://nanojournal.ifmo.ru/news/16/',
-	'http://nanojournal.ifmo.ru/news/10/',
-	'http://nanojournal.ifmo.ru/news/7/',
-	'http://nanojournal.ifmo.ru/news/5/',
-	'http://nanojournal.ifmo.ru/news/3/',
-	'http://nanojournal.ifmo.ru/news/1/'
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper01/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper02/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper03/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper04/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper05/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper06/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/mathematics/paper07/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper08/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper09/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper10/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper11/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper12/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper13/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper14/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper15/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper16/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/physics/paper17/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/chemistry/paper18/',
+	'http://nanojournal.ifmo.ru/en/articles-2/volume8/8-1/chemistry/paper19/',
 ];
 
+var articles = [];
 
 async.forEachOf(urls, function(url, key, callback) {
 	request({uri: url, method:'GET', encoding:'binary'}, function (err, res, page) {
@@ -42,24 +34,23 @@ async.forEachOf(urls, function(url, key, callback) {
 	    var $=cheerio.load(page);
 	    
 	    //Идём по DOM-дереву обычными CSS-селекторами
-	    title=$('h1.entry-title').text();
-	    date=$('div.entry-meta > a > span.entry-date').text();
-	    author=$('div.entry-meta > span.author.vcard > a.url ').text();
-	    content=$('div.entry-content').html();
-	        
-	    var resource  = store.sym('http://example.com/article/' + key);
-	    store.add(resource, FOAF('title'), title);
-		store.add(resource, FOAF('author'), author);
-		store.add(resource, FOAF('date'), $rdf.literal(date, '', XSD('dateTime')));
-		store.add(resource, FOAF('content'), content);
-
-		var wstream = fs.createWriteStream('dump.ttl');
-		$rdf.serialize(undefined, store, 'dump.ttl', 'application/x-turtle', function(err, str) {
-		    if(err) console.log(err);
-		    wstream.write(str);
-		});
-		wstream.end();
+	    title=$('div.entry-content > p > strong').text();
+	    author=$('div.entry-content > p:nth-child(3) ').text();
+	    content=$('div.entry-content > p:nth-child(4)').text();
+	    keyword= $('div.entry-content > p:nth-child(5)').text().replace("Keywords: ", "");;
+	    pdf= $('div.entry-content > p:nth-child(8) > a').attr('href');
+	    
+	    var article = [
+	    	"description": content,
+			"title": title,
+			"annotation_path": url,
+			"keywords_codes": keyword,
+			"article_url": pdf,
+			"authors": author
+	    ]
+	    articles.push(article);
 	});
+	
 	callback();
 
 }, function(err) {
@@ -69,3 +60,5 @@ async.forEachOf(urls, function(url, key, callback) {
     	console.log('success');
     }
 });
+
+JSON.stringify(articles);
